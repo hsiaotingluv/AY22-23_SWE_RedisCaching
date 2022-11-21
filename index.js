f55300b172fd3773fd35d488961f556f33d45e01
Chen Hsiao Ting <hsiaoting0919@gmail.com>
2022-11-21 23:14:19 +0800

    Retrieve data from local MongoDB

--- index.js
+++ index.js
@@ -1,47 +1,81 @@
 const express = require("express");
-const axios = require("axios");
+// const axios = require("axios");
 const redis = require("redis");
+let bodyParser = require("body-parser");
+var MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 const port = process.env.PORT || 3000;
+const url = "mongodb://localhost:27017";
 
-let redisClient;
+let redisClient = redis.createClient();
+redisClient.on("error", (error) => console.error(`Error : ${error}`));
+redisClient.connect().then(console.log("Redis Client Connected!"));
 
-(async () => {
-  redisClient = redis.createClient();
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
-  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+// MongoDB object
+var db = null;
+MongoClient.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      return console.log(err);
+    }
 
-  await redisClient.connect();
-})();
+    // Specify the database you want to access
+    db = client.db("sample_airbnb");
 
-async function fetchApiData(species) {
-  const apiResponse = await axios.get(
-    `https://www.fishwatch.gov/api/species/${species}`
-  );
-  console.log("Request sent to the API");
-  return apiResponse.data;
+    console.log(`MongoDB Connected: ${url}`);
+  }
+);
+
+async function fetchData(propertyType) {
+  // const apiResponse = await axios.get(
+  //   `https://www.fishwatch.gov/api/species/${species}`
+  // );
+  // console.log("Request sent to the API");
+  // return apiResponse.data;
+
+  const airbnb = [];
+  const res = await db.collection("listingsAndReviews").find({}).toArray();
+
+  res.forEach((element) => {
+    airbnb.push(element);
+  });
+
+  return airbnb;
 }
 
-async function getSpeciesData(req, res) {
-  const species = req.params.species;
+async function getAirbnbData(req, res) {
   let results;
   let isCached = false;
 
   try {
-    const cacheResults = await redisClient.get(species);
+    const cacheResults = await redisClient.get("airbnbData");
     if (cacheResults) {
+      console.log("fetching from Redis Client");
       isCached = true;
       results = JSON.parse(cacheResults);
     } else {
-      results = await fetchApiData(species);
+      console.log("fetching from local MongoDB");
+      results = await fetchData();
       if (results.length === 0) {
         throw "API returned an empty array";
       }
-      await redisClient.set(species, JSON.stringify(results));
+      await redisClient.set("airbnbData", JSON.stringify(results));
     }
 
-    res.send({
+    res.status(200).send({
       fromCache: isCached,
       data: results,
     });
@@ -51,7 +85,8 @@ async function getSpeciesData(req, res) {
   }
 }
 
-app.get("/fish/:species", getSpeciesData);
+app.get("/airbnb", getAirbnbData);
+app.get("/", (req, res) => res.send("Hello World with Express"));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
